6089188d017517a1eb2d3476e2a011f9d85339bf
Chiu Yue Chun <chiubrian@link.cuhk.edu.hk>
2016-06-11 18:12:43 +0800

    Add view switching logic

--- app/src/main/java/com/loopbook/cuhk_loopbook/view/ConfirmGoButton.java
+++ app/src/main/java/com/loopbook/cuhk_loopbook/view/ConfirmGoButton.java
@@ -5,13 +5,60 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.loopbook.cuhk_loopbook.R;
 
 public class ConfirmGoButton extends RelativeLayout {
+    private View startButton;
+    private View cancelButton;
+    private View goButton;
+
+    private enum State {READY, CONFIRMING, BUSY}
+
     public ConfirmGoButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.confirm_go, this);
+        bindView(view);
+        setupButons();
+    }
+
+    private void bindView(View view) {
+        startButton = view.findViewById(R.id.fab_start);
+        cancelButton = view.findViewById(R.id.fab_cancel);
+        goButton = view.findViewById(R.id.fab_go);
+    }
+
+    private void setupButons() {
+        startButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchState(State.CONFIRMING);
+            }
+        });
+        cancelButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchState(State.READY);
+            }
+        });
+    }
+
+    private void switchState(State state) {
+        switch (state) {
+        case READY:
+            cancelButton.setVisibility(View.GONE);
+            goButton.setVisibility(View.GONE);
+            startButton.setVisibility(View.VISIBLE);
+            break;
+        case CONFIRMING:
+            cancelButton.setVisibility(View.VISIBLE);
+            goButton.setVisibility(View.VISIBLE);
+            startButton.setVisibility(View.GONE);
+            break;
+        case BUSY:
+            break;
+        }
     }
 }
